60dfb24ecb1e2e318df8ce1c1da268b017ca4417
Jasmine Henry <Jasminehenry356@gmail.com>
2023-03-24 08:53:35 -0400

    update

---
+++ methods.js
@@ -0,0 +1,62 @@
+// // length
+
+// function strLength () {
+//     const string = "Great idea Meaghen!";
+//     console.log(string.length)
+// }
+
+//     const string1 () {
+//         const string = "Great idea Meaghan";
+//         console.log(string.length)
+//     }
+
+ // Length Invoke
+// strLength()
+
+// // To Uppercase
+// function toUpper1() {
+//     const string = "omg what is wrong with me";
+//     console.log("str uppercase:", string.toUpperCase())
+// }
+
+// toUpper1()
+
+// trim
+
+// function strTrim1() {
+//     const string = " hello this is tom       "
+//     console.log("str trim", string.trim())
+// }
+
+// strTrim1();
+
+// Replace 
+
+// function strReplace1 () {
+//     const str = " I didnt sleep much last night";
+//     const subject = "sleep much";
+//     const Replace = "eat anything";
+//     console.log("str replace", str.Replace(subject, Replace))
+// }
+
+// strReplace1()    // not working
+
+
+// Substring
+function strSub1() {
+    const str = "I like big fonts and I cannot lie";
+    console.log("subString", str.substring(7, 15));
+    console.log("subString", str.substring(0, 2))
+}
+
+strSub1()    
+
+// .split()
+
+function strSplit () {
+    const str = "I like star wars";
+    console.log("str Split", str.split(" "))
+}
+
+strSplit()
+
---
+++ project-sentence-converter.js
@@ -0,0 +1,18 @@
+/*
+objective - create a function that converts a string to a freiendly sentence
+
+string method tooLowerCase(), toUpperCase(), trim()
+*/
+
+function converterSentence(text) {
+    const upperText1 = text.toLowerCase();
+    const upperText2 = upperText [0].toUppercase();
+    const finalText = upperText2 + upperText.substring(1);
+    return finalText
+
+}
+
+const sentence = "type better please"
+const result = converterSentence
+
+
